20c32d90a39ada85ee39c063ab1f62254ed564bd
Witoo Harianto <witoo@plimble.com>
2017-06-01 00:52:21 +0700

    remove error

--- pub.go
+++ pub.go
@@ -8,21 +8,13 @@ type Marshaler interface {
 	Marshal() ([]byte, error)
 }
 
-func NewEvent(eventType string, data Marshaler) ([]byte, error) {
-	bdata, err := data.Marshal()
-	if err != nil {
-		return nil, err
-	}
-
+func NewEvent(eventType string, data Marshaler) []byte {
+	bdata, _ := data.Marshal()
 	epb := &pb.Event{
 		Type: eventType,
 		Data: bdata,
 	}
 
-	bepb, err := epb.Marshal()
-	if err != nil {
-		return nil, err
-	}
-
-	return bepb, nil
+	bepb, _ := epb.Marshal()
+	return bepb
 }
